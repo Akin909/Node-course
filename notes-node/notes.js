b9578707f280f70ebfb2e4b312f824c5da62db53
Akin909 <akin.sowemimo@gmail.com>
2017-01-01 10:44:44 +0000

    add saving data functionality

--- notes-node/notes.js
+++ notes-node/notes.js
@@ -1,8 +1,37 @@
 console.log('starting notes.js')
 
+const fs = require('fs');
+
+let fetchNotes = () =>{
+  try{
+    let noteString = fs.readFileSync('notes-data.json');
+    return JSON.parse(noteString);
+  } catch (error){
+    return []
+  }
+}
+
+let saveNotes (notes) => {
+  fs.writeFileSync('notes-data.json',JSON.stringify(notes));
+}
+
 let addNote = (title, body) => {
-  console.log('adding note', title, body);
+  let notes = fetchNotes();
+  let note = {
+    title,
+    body,
+  }
+
+
+
+  let duplicateNotes = notes.filter((note)=>note.title === title);
+
+  if (duplicateNotes.length === 0){
+    notes.push(note);
+
+  }
 };
+
 
 let getAll = () =>{
   console.log('getting all notes');
